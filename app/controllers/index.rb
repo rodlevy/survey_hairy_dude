89d59ee343d92a9d14b976c0ff8b6af03d16f2eb
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 13:22:13 -0500

    change

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,23 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/create' do 
+  if !current_user
+    redirect to '/'
+  else
+    erb :test_jordan
+  end
+end
+
+post '/create' do
+  # survey = Survey.create(:name => params[:survey_name],
+  #                         :user_id => current_user.id)
+  # question = Question.create(:survey_id => survey.id, 
+  #                 :content => params[:question]
+  #                 )
+  # Answer.create(:content => params[:answer],
+  #               :question_id => question.id)
+p params
+  redirect to '/'
+end
